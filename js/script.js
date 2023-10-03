b70645c1f0dcbeaeda6589e806345620cb394cb3
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2023-10-03 06:03:50 +0600

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// 
+'use strict';
+
+document.querySelector('.news').innerHTML = `<i class="ri-checkbox-circle-fill"></i> Correct Number 🎉`
+
+document.querySelector('.sub-heading').innerHTML = 13;
+document.querySelector('.score').innerHTML = 13;
