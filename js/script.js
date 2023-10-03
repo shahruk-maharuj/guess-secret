13a0f8f4223d7218a3470a0b75a83aa3364c9997
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2023-10-03 13:14:31 +0600

    add js for all functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,51 @@
 'use strict';
 
-document.querySelector('.news').innerHTML = `<i class="ri-checkbox-circle-fill"></i> Correct Number 🎉`
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
-document.querySelector('.sub-heading').innerHTML = 13;
-document.querySelector('.score').innerHTML = 13;
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('#num-box').value);
+
+    if (!guess) {
+        document.querySelector('.sub-heading').innerHTML = `<i class="ri-error-warning-fill"></i> No input Number 😔`
+    } else if (guess === secretNumber) {
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.hscore').innerHTML = highScore;
+        }
+        document.querySelector('.sub-heading').innerHTML = `<i class="ri-checkbox-circle-fill"></i> ${guess} - Correct Number 🎉`;
+        document.querySelector('body').style.backgroundColor = `#bef264`;
+        document.querySelector('.number').innerHTML = secretNumber;
+
+    } else if (guess > secretNumber) {
+        if (score > 0) {
+            document.querySelector('.sub-heading').innerHTML = `${guess} - Too High 📈`
+            score--;
+            document.querySelector('.score').innerHTML = score;
+        } else {
+            document.querySelector('.sub-heading').innerHTML = `You lost the game! 😑`
+            document.querySelector('.score').innerHTML = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 0) {
+            document.querySelector('.sub-heading').innerHTML = `${guess} - Too low 📉`
+            score--;
+            document.querySelector('.score').innerHTML = score;
+        } else {
+            document.querySelector('.sub-heading').innerHTML = `You lost the game! 😑`
+            document.querySelector('.score').innerHTML = 0;
+        }
+    }
+
+});
+
+// window refresh = location.reload();
+document.querySelector('.try').addEventListener('click', () => {
+    /* score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.score').innerHTML = score;
+    document.querySelector('.number').innerHTML = `?`;
+    document.querySelector('#num-box').value = ''; */
+    location.reload()
+})
